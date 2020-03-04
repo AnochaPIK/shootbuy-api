933aa8a26ef83521995318db95d5609dd8777b80
AnochaPIK <anochapik@gmail.com>
2020-03-04 16:48:10 +0700

    td for loop find price each product

--- src/product-order/product-order.service.ts
+++ src/product-order/product-order.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './models/order/order.entity';
-import { Repository } from 'typeorm';
+import { Repository, getRepository } from 'typeorm';
 import { OrderDetail } from './models/order-detail/order-detail.entity';
+import { FoodAndBev } from 'src/product-data/models/food-and-bev/food-and-bev.entity';
 
 @Injectable()
 export class ProductOrderService {
@@ -17,7 +18,57 @@ export class ProductOrderService {
             var orderDetail = new OrderDetail()
             orderDetail = order.orderDetail[0]
             orderDetail.orderId = selectOrder[0].orderId
-            const insertOrderDetail = await this.orderDetailRepository.save(orderDetail)
+            const orderDetailIsExist = await this.orderDetailRepository.find(orderDetail)
+            if (orderDetailIsExist.length > 0) {
+                orderDetailIsExist[0].quantity++
+
+                await this.orderDetailRepository.save(orderDetailIsExist)
+            }
+            else{
+                await this.orderDetailRepository.save(orderDetail)
+            }
+            
+            console.log( selectOrder[0].orderDetail)
+
+
+            
+
+
+
+
+
+            // const selectProductOrderDetail = await this.orderDetailRepository.find({
+            //     relations: ["product"],
+            //     where: {
+            //         orderId: orderDetail.orderId,
+            //         productId: orderDetail.productId
+            //     }
+            // })
+            // const categoryId = selectProductOrderDetail[0].product.categoryId
+            // if (categoryId == 1) {
+            //     const foodAndBevData = await getRepository(FoodAndBev).find({
+            //         where: {
+            //             foodAndBevId: selectProductOrderDetail[0].product.productId
+            //         }
+            //     })
+            //     var totalFoodAndBevPrice = foodAndBevData[0].foodAndBevPrice * selectProductOrderDetail[0].quantity
+            //     const selectProductOrder = await this.orderRepository.find({
+            //         where: {
+            //             orderId: selectOrder[0].orderId,
+            //         }
+            //     })
+            //     var updateOrder = new Order()
+            //     updateOrder = selectProductOrder[0]
+            //     updateOrder.totalPrice += totalFoodAndBevPrice
+            //     await this.orderRepository.save(updateOrder)
+
+            // }
+            // else if (categoryId == 2) {
+
+            // }
+            // else if (categoryId == 3) {
+
+            // }
         }
         else if (selectOrder.length == 0) {
             const insertOrder = await this.orderRepository.save(order)
@@ -28,7 +79,7 @@ export class ProductOrderService {
         }
     }
 
-    async getProductOrder(email) : Promise<any[]>{
+    async getProductOrder(email): Promise<any[]> {
         const selectProductOrder = await this.orderRepository.find({
             relations: ["orderDetail"],
             where: {
